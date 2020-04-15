35a649952af09514dc04225464475baf8d404008
Jarrett Byrnes <jebyrnes@Jarretts-MacBook-Air-2.local>
2020-04-14 22:47:40 -0400

    Update tests with location_code

--- R/refresh_YOURPACKAGENAME.R
+++ R/refresh_YOURPACKAGENAME.R
@@ -11,8 +11,8 @@
 #' * date - The date in YYYY-MM-DD form
 #' * location - The name of the location as provided by the data source. XXXXX
 #' * location_type - The type of location using the covid19R controlled vocabulary. SAY MORE ABOUT THIS
-#' * location_standardized - A standardized location code using a national or international standard. ADD MORE
-#' * location_standardized_type The type of standardized location code being used according to the covid19R controlled vocabulary. Here we use `XXXX`
+#' * location_code - A standardized location code using a national or international standard. ADD MORE
+#' * location_code_type The type of standardized location code being used according to the covid19R controlled vocabulary. Here we use `XXXX`
 #' * data_type - the type of data in that given row using the covid19R controlled vocabulary. Includes WHAT DATA TYPES ARE HERE?
 #' * value - number of cases of each data type
 #' @export refresh_YOURPACKAGENAME
