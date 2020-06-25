1dc992f24e1339b33f8e6a48184b700d941ac429
jebyrnes <jarrett.byrnes@umb.edu>
2020-06-25 12:45:03 -0400

    Add new tests.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,20 @@
 library(testthat)
+library(glue) #for later use
 library(YOURPACKAGENAME)
+
+
+# A tibble of refresh functions for different data sets
+# and the number of rows when you refresh on a particular
+# start date. The functions will be used in other tests. 
+# the length and date will be used to make sure the dataset
+# is getting longer over time when you build the package again
+# and not shorter! As you'd have to dig in and rewrite a bit if
+# the former is true
+refresh_funs <- dplyr::tribble(
+  ~fun, ~len, ~date_check,
+  "refresh_FUN_1", NROW_WHEN_YOU_START, "DATE_YOU_STARTED"#,
+  #more refresh functions if you have them
+)
+
 
 test_check("YOURPACKAGENAME")
---
+++ tests/testthat/test-test_controlled_vocab.R
@@ -0,0 +1,90 @@
+# things to test against
+refresh_col_names <-
+  c(
+    "date",
+    "location",
+    "location_type",
+    "location_code",
+    "location_code_type",
+    "data_type",
+    "value"
+  )
+
+#where the controlled vocabs live
+location_types <- read.csv("https://github.com/Covid19R/covid19Rdata/raw/master/controlled_vocabularies/location_type.csv", stringsAsFactors = FALSE)[, 1]
+location_code_types <- read.csv("https://github.com/Covid19R/covid19Rdata/raw/master/controlled_vocabularies/location_code_type.csv", stringsAsFactors = FALSE)[, 1]
+data_types <- read.csv("https://github.com/Covid19R/covid19Rdata/raw/master/controlled_vocabularies/data_type.csv", stringsAsFactors = FALSE)[, 1]
+
+# helper function
+
+expect_contains <- function(vec1, vec2) {
+  vec1 <- unique(vec1)
+  
+  expect(
+    all(vec1 %in% vec2),
+    paste0(
+      "Some or all values of ",
+      paste0(vec1, collapse = ","),
+      "\ndo not match ",
+      paste0(vec2, collapse = ","),
+      "\nIf this is a new controlled vocabulary entry ",
+      "please file an issue at\nhttps://github.com/Covid19R/covid19R/issues"
+    )
+  )
+}
+
+
+# OK, the testing!
+
+# the function to run the tests
+
+test_one_refresh_for_vocab <- function(arow) {
+  
+  res <- eval(call(arow$fun))
+  
+  # make sure there's there there
+  expect_gt(nrow(res), 0)
+  
+  last_col <- 7
+  
+  # if you have a refresh function that returns more than the standard
+  # cols, use an if statement to snip it off! use | for more than 1
+  # refresh function
+  # make sure column names are in order
+  # if (arow$fun == "refresh_FUN") {
+  #   refresh_col_names <-
+  #     c(
+  #       "date",
+  #       "location",
+  #       "location_type",
+  #       "location_code",
+  #       "location_code_type",
+  #       "state",
+  #       "data_type",
+  #       "value"
+  #     )
+  #   
+  #   last_col <- 8
+  # }
+  
+  expect_named(res[, 1:last_col], refresh_col_names) # allow for other cols
+  
+  # loc types
+  expect_contains(res$location_type, location_types)
+  
+  # loc codes
+  expect_contains(res$location_code_type, location_code_types)
+  
+  # dat types
+  expect_contains(res$data_type, data_types)
+}
+
+# run all tests
+
+# check the number of rows is greater than it was when the
+# function was added to the package
+for (i in seq_len(nrow(refresh_funs))) {
+  test_that(glue::glue("{refresh_funs[i,]$fun} works and has propercontrolled format and vocab"), {
+    test_one_refresh_for_vocab(refresh_funs[i, ])
+  })
+}
